3a0d3240a3a98e099b765d952d8cbcfd13675bb6
jesussantana <jesus@esmarketingdigital.com>
2021-07-04 06:58:21 +0200

    Project Organization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Skyrocket your SQL skills by performing over 150 analysis tasks',
+    author='Jesus Santana',
+    license='MIT',
+)
